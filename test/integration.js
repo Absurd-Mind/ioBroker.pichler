4128ed0b88cca368c9afde02423af8cfe6ff93f4
Peter Kranz <peter@myref.net>
2026-06-15 15:01:13 +0200

    refactor: add environment variable for non-blocking engine checks in integration tests

--- test/integration.js
+++ test/integration.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const { tests } = require("@iobroker/testing");
 
+// The integration test harness installs the adapter in a temp directory.
+// Keep engine checks non-blocking there so Node 18 CI jobs don't fail on transitive deps.
+process.env.NPM_CONFIG_ENGINE_STRICT = "false";
+
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.integration(path.join(__dirname, ".."), {
 	controllerVersion: "7.2.1",
